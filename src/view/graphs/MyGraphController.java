2e55d257a88974728f9ae939d0cd78548eb0151d
Bar Shaya <74703704+barshaya@users.noreply.github.com>
2021-05-29 19:33:00 +0300

    Revert "Bar"

--- src/view/graphs/MyGraphController.java
+++ src/view/graphs/MyGraphController.java
@@ -1,17 +1,4 @@
 package view.graphs;
-import javafx.fxml.FXML;
-import javafx.scene.chart.LineChart;
-import javafx.scene.chart.BubbleChart;
 
 public class MyGraphController {
-
-    @FXML LineChart CorrelatedChart;
-    @FXML LineChart FeatureChart;
-    @FXML BubbleChart algoChart;
-
-
-    public MyGraphController() {
-        super();
-
-    }
-    }
+}
--- src/view/viewlist/MyViewListController.java
+++ src/view/viewlist/MyViewListController.java
@@ -1,15 +1,4 @@
 package view.viewlist;
 
-import javafx.fxml.FXML;
-import javafx.scene.control.ListView;
-
 public class MyViewListController {
-
-        @FXML
-        ListView listView;
-
-        public MyViewListController() {
-        }
-
-
 }
