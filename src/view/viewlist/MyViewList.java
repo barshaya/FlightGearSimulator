bc6c5dd70d976c1b3a06f42206c22d3987ade3c2
barshaya <barshaya97@gmail.com>
2021-05-29 19:43:56 +0300

    GuiIsWorking

--- src/view/viewlist/MyViewList.java
+++ src/view/viewlist/MyViewList.java
@@ -1,18 +1,12 @@
 package view.viewlist;
 
 import java.io.IOException;
-
-import javafx.beans.property.StringProperty;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
-import javafx.scene.control.MenuItem;
 import javafx.scene.layout.AnchorPane;
 
 public class MyViewList extends AnchorPane {
     public ListView list;
-
-    public StringProperty xmlpath;
 
     public MyViewList() {
         try {
@@ -20,9 +14,6 @@ public class MyViewList extends AnchorPane {
             AnchorPane viewlist = (AnchorPane)fxl.load(this.getClass().getResource("MyViewList.fxml").openStream());
             MyViewListController myViewListController = (MyViewListController)fxl.getController();
             this.list = myViewListController.listView;
-
-            xmlpath = myViewListController.XmlPath;
-
             this.getChildren().add(viewlist);
         } catch (IOException var4) {
             var4.printStackTrace();
--- src/view/viewlist/MyViewListController.java
+++ src/view/viewlist/MyViewListController.java
@@ -1,41 +1,16 @@
 package view.viewlist;
 
-import javafx.beans.property.SimpleStringProperty;
-import javafx.beans.property.StringProperty;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
-import javafx.scene.control.MenuItem;
-import javafx.stage.FileChooser;
-
-import java.io.File;
 
 public class MyViewListController {
-
 
         @FXML
         ListView listView;
 
-
-        StringProperty XmlPath;
-
         public MyViewListController() {
                 super();
-                XmlPath = new SimpleStringProperty();
         }
 
-        public void openXMLFile() {
-                FileChooser fc = new FileChooser();
-                fc.setTitle("File Choose");
-                fc.setInitialDirectory(new File("./resources"));
-                FileChooser.ExtensionFilter ef = new FileChooser.ExtensionFilter("XML Files (*.xml)", "*.xml");
-                fc.getExtensionFilters().add(ef);
-                File chosen = fc.showOpenDialog(null);
-                if (chosen != null) {
-                        //vm.loadXml("resources/"+chosen.getName());
-                        XmlPath.setValue("resources/" + chosen.getName());
-
-                }
-        }
 
 }
